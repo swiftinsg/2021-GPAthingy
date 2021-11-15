b838f52028a3ac724f653c6bfbddb1f77c94cdb3
cpyoufy <cpyoufy@gmail.com>
2021-11-15 12:12:15 +0800

    Initial Commit

---
+++ GPAthingy/GPAthingyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPAthingyApp.swift
+//  GPAthingy
+//
+//  Created by rgs on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GPAthingyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

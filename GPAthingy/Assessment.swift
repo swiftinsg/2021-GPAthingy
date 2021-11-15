b8283a32c8d954c00e27301d23aac0c9d49b02b0
cpyoufy <cpyoufy@gmail.com>
2021-11-15 14:00:58 +0800

    Added Assessment Identifiable (Not done)

---
+++ GPAthingy/Assessment.swift
@@ -0,0 +1,15 @@
+//
+//  Assessment.swift
+//  GPAthingy
+//
+//  Created by cpyoufy on 15/11/21.
+//
+
+import Foundation
+
+
+struct Assessment: Identifiable {
+    var id = UUID()
+    
+    var name: String
+}
--- GPAthingy/GPAthingyApp.swift
+++ GPAthingy/GPAthingyApp.swift
@@ -2,7 +2,7 @@
 //  GPAthingyApp.swift
 //  GPAthingy
 //
-//  Created by rgs on 15/11/21.
+//  Created by cpyoufy on 15/11/21.
 //
 
 import SwiftUI

1d8f1bf2ce38f0fc4862e603ba60a0e9bc82d77c
cpyoufy <cpyoufy@gmail.com>
2021-11-16 13:46:49 +0800

    Buggy nested lists

--- GPAthingy/SubjectDetailView.swift
+++ GPAthingy/SubjectDetailView.swift
@@ -1,23 +1,50 @@
 //
 //  SubjectDetailView.swift
-//  GPAthingy
 //
-//  Created by rgs on 16/11/21.
+//  Created by cpyoufy on 13/11/21.
 //
 
 import SwiftUI
 
 struct SubjectDetailView: View {
+    var assessment: Assessment;
     
-    var assessment: Assessment
+    var subject = [Subject(name: "Math"),
+                      Subject(name: "English")]
     
+    let listItemColor = Color(red: 245 / 255, green: 239 / 255, blue: 255 / 255)
     var body: some View {
-        Text(assessment.name)
+        NavigationView {
+            List {
+                ForEach(subject) { subject in
+                    VStack(alignment: .leading) {
+                        NavigationLink(destination: ActualSubjectDetailView(subject: subject)) {
+                            HStack {
+                                HStack (alignment: .top){
+                                    Text( subject.name)
+                                        .bold()
+                                }
+                                Spacer()
+                                HStack (alignment: .bottom) {
+                                    Text("69%")
+                                }
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                } .listRowBackground(listItemColor)
+                
+                .padding()
+                
+            } .navigationTitle("Subjects")
+        }
     }
 }
 
 struct SubjectDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        SubjectDetailView(assessment: Assessment(name: "Assessment 1"))
+        SubjectDetailView(assessment:  Assessment(name: "Assessment 1"))
     }
 }

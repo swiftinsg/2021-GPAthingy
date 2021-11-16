68e8ad99bd408840938b6803e503db70772873c8
cpyoufy <cpyoufy@gmail.com>
2021-11-16 12:19:54 +0800

    Added colour for list items and circle layout -- Not finished

--- GPAthingy/GoalsScreen.swift
+++ GPAthingy/GoalsScreen.swift
@@ -9,22 +9,43 @@ import SwiftUI
 
 struct GoalsScreen: View {
     
+    let listItemColor = Color(red: 245 / 255, green: 239 / 255, blue: 255 / 255)
+    
     var todo = [ToDo(name: "Read Math Textbook"),
-                   ToDo(name: "Holiday Homework"),
-                   ToDo(name: "Read Up On Thesis")]
+                ToDo(name: "Holiday Homework"),
+                ToDo(name: "Read Up On Thesis")]
     
     var body: some View {
-        List(todo) { todo in
-            VStack(alignment: .leading) {
-                Text( todo.name)
-                    .bold()
-            }
+        List {
+            ForEach(todo) { todo in
+                VStack(alignment: .leading) {
+                    HStack {
+                        
+                            Text( todo.name)
+                        .bold()
+                                .frame(alignment: .leading)
+                        
+                        
+                        Circle()
+                            .frame(width: 28, height: 18, alignment: .trailing)
+                        
+                        
+                    }
+                }
+                
+            } .listRowBackground(listItemColor)
+            
+            .padding()
+            
+            
+            
+            
         }
     }
-}
-
-struct GoalsScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct GoalsScreen_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }

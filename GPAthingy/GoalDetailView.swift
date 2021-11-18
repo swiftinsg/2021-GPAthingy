fd780e1a294363b858a7ad978af587a779bb40bb
cpyoufy <cpyoufy@gmail.com>
2021-11-18 13:15:59 +0800

    Xcode crash skem

--- GPAthingy/GoalDetailView.swift
+++ GPAthingy/GoalDetailView.swift
@@ -9,45 +9,48 @@ import SwiftUI
 
 struct GoalDetailView: View {
     
+    let CircleColorRed = Color(red: 255 / 255, green: 127 / 255, blue: 127 / 255)
+    let CircleColorGreen = Color(red: 184 / 255, green: 243 / 255, blue: 213 / 255)
+    let CircleColorYellow = Color(red: 255 / 255, green: 198 / 255, blue: 0 / 255)
+    
     @Binding var todo: ToDo
     
     var body: some View {
         Form {
-            Section(header: Text("goal title")) {
-                TextField("Whats your goal?", text: $todo.title)
+            Section(header: Text("Goal Title")) {
+                TextField("What's your goal?", text: $todo.title)
             }
             
             Section(header: Text("priority")) {
                 HStack {
                     Button{
-                    
+                        todo.priority = "CircleColorRed"
                     } label: {
                         Image(systemName: "circle.fill")
-                            .foregroundColor(circleColor)
+                            .foregroundColor(CircleColorRed)
                 }
             
                     Button{
-                    
+                        todo.priority = "CircleColorYellow"
                     } label: {
                         Image(systemName: "circle.fill")
-                            .foregroundColor(CircleColourYellow)
+                            .foregroundColor(CircleColorYellow)
                 }
                     Button{
-                    
+                        todo.priority = "CircleColorGreen"
                     } label: {
-                        
-                    }
                         Image(systemName: "circle.fill")
-                            .foregroundColor(CircleColourGreen)
+                            .foregroundColor(CircleColorGreen)
+                    }
                 }
-            }
+            } .buttonStyle(.plain)
         }
-        .navigationTitle(ToDo.title)
+        .navigationTitle(todo.title)
     }
 }
 
 struct GoalDetailView_Previews: PreviewProvider {
     static var previews: some View{
-        GoalDetailView(ToDo: .constant(ToDo(title: "ignore yi kai", priority: .high)))
+        GoalDetailView(todo: .constant(ToDo(title: "ignore yi kai", priority: "CircleColorGreen")))
     }
 }

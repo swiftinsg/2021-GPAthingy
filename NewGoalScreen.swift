c67a023f01548a7e15427ccf756d20465d4e30a5
yashvi <yashvi0106@gmail.com>
2021-11-19 15:14:55 +0800

    before  settings

---
+++ NewGoalScreen.swift
@@ -0,0 +1,25 @@
+//
+//  NewGoalScreen.swift
+//  GPAthingy
+//
+//  Created by Yashvi Shah on 19/11/21.
+//
+
+import SwiftUI
+
+struct ScoreView: View {
+    
+    var score: Int
+    var totalQuestions: Int
+    
+    var body: some View {
+        VStack {
+           /*TextField("Score", text: $score)
+                .keyboardType(.decimalPad)
+                .padding()
+                .background(Color_cdc1ff)
+                .cornerRadius(16)*/
+
+        }
+    }
+}

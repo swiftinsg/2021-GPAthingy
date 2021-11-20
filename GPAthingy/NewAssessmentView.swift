9696d7e5115c43de0a4a842afb1af22db97875f5
riteshideas <ritesh.sathiyamoorthi@gmail.com>
2021-11-20 23:47:29 +0800

    Made the Add Subject UI in the `NewAssesmentView`

--- GPAthingy/NewAssessmentView.swift
+++ GPAthingy/NewAssessmentView.swift
@@ -10,8 +10,94 @@
 import SwiftUI
 
 struct NewAssessmentView: View {
+    
+    @State var subjectName = ""
+    @State var score = ""
+    @State var scoreInt = 0.0
+    @State var totalScore = ""
+    @State var totalScoreInt = 0.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("SUBJECT NAME")
+                .font(.system(size: 13))
+                .bold()
+            
+            TextField("Meth", text: $subjectName)
+                .padding()
+                .background(Color_F5EFFF)
+                .cornerRadius(15)
+            
+            
+            Text("SCORE")
+                .font(.system(size: 13))
+                .bold()
+            
+            TextField("69", text: $score)
+                .keyboardType(.decimalPad)
+                .onChange(of: score) { newValue in
+                    
+                    if score.count > 8 {
+                        score.removeLast()
+                    }
+                    
+                    if let score = Double(score) {
+                        scoreInt = score
+                    }
+                }
+                .padding()
+                .background(Color_F5EFFF)
+                .cornerRadius(15)
+            
+            Text("TOTAL SCORE")
+                .font(.system(size: 13))
+                .bold()
+            
+            TextField("40269", text: $totalScore)
+                .keyboardType(.decimalPad)
+                .onChange(of: totalScore) { newValue in
+                    
+                    if totalScore.count > 8 {
+                        totalScore.removeLast()
+                    }
+                    
+                    if let score = Double(totalScore) {
+                        totalScoreInt = score
+                    }
+                }
+                .padding()
+                .background(Color_F5EFFF)
+                .cornerRadius(15)
+            
+            
+            Text("PERCENTAGE")
+                .font(.system(size: 13))
+                .bold()
+            
+            Text("\((scoreInt / totalScoreInt) * 100, specifier: "%.1f")%")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .background(Color_F5EFFF)
+                .cornerRadius(15)
+
+            
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Spacer()
+                Text("Save")
+                Spacer()
+            }
+            .font(.system(size: 22, weight: .bold))
+            .foregroundColor(.white)
+            .padding()
+            .background(Color_7371fc)
+            .cornerRadius(15)
+        }
+        .padding()
     }
 }
 
